8e610671a755e562d63cf22ed9e7f38af9e54c0f
jennysj1 <146471240+jennysj1@users.noreply.github.com>
2024-02-23 12:09:17 -0800

    added tests to sample

--- calculator/tests/01_example.sh
+++ calculator/tests/01_example.sh
@@ -29,3 +29,21 @@ if $CALCULATOR 3 @ 2; then  # If the return code of $PROGRAM is zero (i.e. succe
   echo 'ERROR! An invalid run of the application (3 @ 2) apparently succeeded?!'
   exit 1
 fi
+
+# Test 04: Ensure simple division case has correct output...
+if [[ $($CALCULATOR 10 / 5) -ne 2 ]]; then  # If the output of the program is not 2...
+  echo 'ERROR! A valid run of the calculator (10 / 5) failed to produce 2 as an output!'
+  exit 1
+fi
+
+# Test 05: Ensure simple multiplication case has correct output...
+if [[ $($CALCULATOR 2 '*' 3) -ne 6 ]]; then
+  echo 'ERROR! A valid run of the calculator (2 * 3) failed to produce 6 as an output!'
+  exit 1
+fi
+
+# Test 06: Ensure simple subtraction case has correct output...
+if [[ $($CALCULATOR 5 - 3) -ne 2 ]]; then
+  echo 'ERROR! A valid run of the calculator (5 - 3) failed to produce 2 as an output!'
+  exit 1
+fi
